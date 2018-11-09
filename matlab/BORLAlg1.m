3ab8b8a28d218c2f23b27717120421a8d9e07869
Ashwin Rajendra Khadke <akhadke@andrew.cmu.edu>
2018-11-08 20:24:20 -0500

    BORLAlg1 in progress

--- matlab/BORLAlg1.m
+++ matlab/BORLAlg1.m
@@ -1,25 +1,32 @@
 classdef BORLAlg1 < handle
     properties
-        dynamics % function that takes a vector of states and a vector of actions and returns a vector of next states
-        reward   % cost
+        env
         muQ      % bootstrapped mean function for Q values, takes in the state and the action
         sigmaQ   % bootstrapped sigma function for Q values, takes in the state and action
         policy
+        actions
     end
     
     methods
-        function borl = BORLAlg1(dynamics, reward)
-            borl.dynamics = dynamics;
-            borl.reward = reward;
-            
+        function borl = BORLAlg1(env)
+            borl.env = env;
         end
         
-        function policy = train_policy(maxiter, horizon, debug)
+        function policy = train_policy(borl, maxiter, horizon, debug_)
             iter = 0;
             while(iter<maxiter)
-                iter = iter+1;
-                for t=1:1:horizon
-                    
+                if (debug_)
+                    iter = iter+1
+                else
+                    iter = iter+1;
+                end
+                t = 0;
+                state = borl.env.reset();
+                is_terimnal = false;
+                while(~is_terminal && t<horizon)
+                    t = t+1;
+                    Q = borl.muQ(state,borl.actions) + randn(size(borl.actions,1),1).*borl.sigmaQ(state,borl.actions);
+                    [minQ, minA] = min(Q);
                 end
             end
         end
---
+++ matlab/swingUpBOAlg1.m
@@ -0,0 +1,69 @@
+clear;
+close all;
+clc;
+
+%% Parameters
+m = 1;
+dt = 0.1;
+numPointsx = 51;
+numPointsx_dot = 51;
+numPointsu = 21;
+x_limits = [0,2*pi];   % Don't change limits for x as the values are wrapped around when an 
+                       % action takes the system beyond 2pi or below 0 and any other
+                       % limts physically don't make sense under this implementation
+                       % If you change numPointsx ensure that goal x is a
+                       % point on the grid i.e. x_limits(1):dx:x_limits(1) has goal x
+
+x_dot_limits = [-5,5]; % Choose limits and numpoints for x_dot such that goal x_dot is a point on the grid 
+                       % i.e. x_dot_limits(1):dx_dot:x_dot_limits(2) has goal_x_dot
+Q = 0.6*eye(2);
+u_limits = [-5,5];
+R = 1;
+gtol = 0.001;
+goal = [pi;0]; 
+start = [0;0];
+gamma_ = 1;
+maxiter = 30;
+max_policy_iter = 30;
+visualize = false;
+test_policy = false;
+
+%% Compute policy using value iteration
+[p, V] = ValueIterationSwingUp(numPointsx, numPointsx_dot, numPointsu, x_limits, x_dot_limits, u_limits,...
+                               Q, R, goal', start', gamma_, gtol, dt, maxiter, max_policy_iter, visualize, test_policy);
+
+imagesc(V);
+xlabel('theta'); ylabel('theta-dot');
+title('Value Iteration');
+colorbar;
+pause(0.5);
+
+%% Run GPDP
+
+dx = (x_limits(2)-x_limits(1))/(numPointsx - 1); 
+dx_dot = (x_dot_limits(2)-x_dot_limits(1))/(numPointsx_dot - 1);
+du  = (u_limits(2)-u_limits(1))/(numPointsu-1);
+[grid_x,grid_x_dot] = meshgrid(x_limits(1):dx:x_limits(2),x_dot_limits(1):dx_dot:x_dot_limits(2));
+env = pendulum(m, dt, x_limits, numPointsx, x_dot_limits, numPointsx_dot, numPointsu, Q, R, goal);
+
+dynamics = @(s,a)env.dynamics(s,a);
+reward = @(s_,a)env.cost(s_,a);
+states = [reshape(grid_x,size(grid_x,1)*size(grid_x,2),1), reshape(grid_x_dot,size(grid_x_dot,1)*size(grid_x_dot,2),1)];
+actions = [u_limits(1):du:u_limits(2)]';
+V_ = zeros(size(states,1),1);
+Q_ = zeros(size(states,1),size(actions,1));
+
+gpdp = GPDP(dynamics, reward, states, actions, V_, Q_, gamma_);
+debug_ = true;
+profile on;
+gpdp.build_policy(500, debug_);
+profile off;
+figure;
+imagesc(reshape(gpdp.muV,size(grid_x,1),size(grid_x,2)));
+colorbar;
+
+%% Run BOAlg1 for a slightly different system with the bootstrapped muQ
+
+
+
+            
